c33f10d91e01c56e48a06f29b97f7a13699df155
Mason Olson <mason14lee@gmail.com>
2026-02-19 14:29:45 -0600

    step 4 done

--- app.js
+++ app.js
@@ -62,6 +62,16 @@ app.post('/contact/submit', (req, res) => {
   res.send("<h1>TODO: Handle contact form posts</h1>" + JSON.stringify(req.body));
 });
 
+app.get("/404", (req, res) => {
+  res.status(404);
+  res.render('default-layout', {
+     title: "Page Not Found",
+     content: "<h1>Sorry!</h1><h3>We can't find the page you're requesting.</h3>"
+  });
+});
+
+
+
 // START THE SERVER
 const server = app.listen(port, () => {
    console.log("Waiting for requests on port %s", port);
